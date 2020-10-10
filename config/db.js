9b2d835b2c4a50939d90cec6139fd2f80a5f5d5e
utkarsh2711 <utkarsh2711@users.noreply.github.com>
2020-10-10 15:24:16 +0530

    One more exceptions added for mongoose

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     await mongoos.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     })
 
     console.log('Mongo DB Connected...')
