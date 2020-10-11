1761107be6d5c0b7e95078c03a494dbad5bc4346
utkarsh2711 <utkarsh2711@users.noreply.github.com>
2020-10-12 00:43:11 +0530

    added function to create or add post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,34 @@
-var postsAPI = (req, res) => {
-  res.status(200).json({ msg: `Hello From Posts Controller` })
+const { validationResult } = require('express-validator')
+const Post = require('../models/Post')
+const User = require('../models/User')
+const Profile = require('../models/Profile')
+
+// @ To CREATE A POST
+var createPost = async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    })
+
+    const post = await newPost.save()
+
+    res.json(post)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json({ msg: 'Internal Server Error' })
+  }
 }
 
 module.exports = {
-  postsAPI,
+  createPost,
 }
