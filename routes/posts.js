2fe52ef11a10c68b5598f69c4f61dae8d330565a
utkarsh2711 <utkarsh2711@users.noreply.github.com>
2020-10-12 00:42:38 +0530

    Added route to create Post

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,18 @@
 const express = require('express')
-
-const postsController = require('../controllers/postsController')
+const auth = require('../config/auth')
+const { check, validationResult } = require('express-validator')
 
 const router = express.Router()
 
-// @route   GET mytweet/api/post
-// @desc    Posts Route
-// @access  Public
-router.get('/posts', postsController.postsAPI)
+const postsController = require('../controllers/postsController')
+
+// @route   POST mytweet/api/post
+// @desc    Create Post
+// @access  Private
+router.post(
+  '/post',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  postsController.createPost
+)
 
 module.exports = router
